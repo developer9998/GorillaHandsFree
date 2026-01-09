0e0db95abfa678af6b74dda19d8a739d02314618
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2026-01-09 16:48:16 -0330

    Update Constants.cs

--- GorillaHandsFree/Constants.cs
+++ GorillaHandsFree/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "dev.gorillahandsfree";
         public const string Name = "GorillaHandsFree";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
